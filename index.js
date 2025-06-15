e14eef5379542572bb5f7a54e5399f17ed77b7af
Your Name <you@example.com>
2025-06-15 22:06:42 +0600

    JavaScript CGPA Calculator first part

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-console.log("Hello world");
-let a= 10;
-console.log(a);
+
+let bangla=100;
+if (33>bangla){
+    console.log('Fail')
+}else if (33<= bangla&& bangla <=39){
+    console.log('D');
+}else if (40<= bangla && bangla <=49){
+    console.log ('C')
+}else if (50<= bangla && bangla <=59){
+    console.log('B')
+}else if (60<= bangla && bangla <=69){
+    console.log('A-')
+}else if (70<= bangla && bangla <=79){
+    console.log('A')
+}else if (80<=bangla){
+    console.log('A+')
+}else{
+    console.log('invalid Mark')
+}
+let english=50;
+if (33>english){
+    console.log('Fail')
+}else if (33<= english&& english <=39){
+    console.log('D');
+}else if (40<= english && english <=49){
+    console.log ('C')
+}else if (50<= english && english <=59){
+    console.log('B')
+}else if (60<= english && english <=69){
+    console.log('A-')
+}else if (70<= english && english <=79){
+    console.log('A')
+}else if (80<=english){
+    console.log('A+')
+}else{
+    console.log('invalid Mark')
+}
